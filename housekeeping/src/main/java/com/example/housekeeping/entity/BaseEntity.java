cb4e096165ab47c128cdc64f04a800dee6835beb
a637814143 <140132912+a637814143@users.noreply.github.com>
2025-10-04 14:01:04 +0800

    Expose auditing timestamps on BaseEntity

--- housekeeping/src/main/java/com/example/housekeeping/entity/BaseEntity.java
+++ housekeeping/src/main/java/com/example/housekeeping/entity/BaseEntity.java
@@ -1,18 +1,17 @@
 package com.example.housekeeping.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
  * 基础实体类
  * 包含公共字段：创建时间、更新时间
  */
-@Data
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
@@ -24,4 +23,45 @@ public abstract class BaseEntity {
     @LastModifiedDate
     @Column(name = "update_time", nullable = false)
     private LocalDateTime updateTime;
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public LocalDateTime getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(LocalDateTime updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(createTime, that.createTime) && Objects.equals(updateTime, that.updateTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(createTime, updateTime);
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "createTime=" + createTime +
+                ", updateTime=" + updateTime +
+                '}';
+    }
 }
