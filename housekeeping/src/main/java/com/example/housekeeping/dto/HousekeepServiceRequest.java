590f6457a0e79b826f6d6ce838eea1b0cc290f36
a637814143 <140132912+a637814143@users.noreply.github.com>
2025-12-05 17:42:03 +0800

    Import NotBlank for HousekeepServiceRequest

--- housekeeping/src/main/java/com/example/housekeeping/dto/HousekeepServiceRequest.java
+++ housekeeping/src/main/java/com/example/housekeeping/dto/HousekeepServiceRequest.java
@@ -1,6 +1,7 @@
 package com.example.housekeeping.dto;
 
 import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
