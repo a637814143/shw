952d26b48c77e10502cb5ff701e508aa7ae4069d
shw866 <1816615650@qq.com>
2026-01-05 21:34:28 +0800

    精选推荐

--- housekeeping-web/src/views/UserDashboardView.vue
+++ housekeeping-web/src/views/UserDashboardView.vue
@@ -2670,12 +2670,28 @@ onUnmounted(() => {
 
 .discover-grid {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
-  gap: 1.5rem;
+  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+  align-items: stretch;
+  gap: 1.25rem;
+}
+
+@media (min-width: 1080px) {
+  .discover-grid {
+    grid-template-columns: 2fr 1fr 1fr;
+    grid-auto-flow: column;
+  }
+  .carousel {
+    grid-column: 1 / 2;
+  }
+  .tips {
+    grid-column: 2 / 3;
+  }
+  .announcements {
+    grid-column: 3 / 4;
+  }
 }
 
 .carousel {
-  grid-column: span 2;
   display: flex;
   flex-direction: column;
   gap: 1rem;
